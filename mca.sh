cbea6ffaeabd5391640ed0f4070abbeafb7f7279
Cody Finegan <44886+codyfinegan@users.noreply.github.com>
2024-12-12 09:57:12 +1300

    Updating version

--- mca.sh
+++ mca.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-java -jar ~/tools/mcaselector/mcaselector-2.3.jar --world ~/server/chromaticchaos2 "$@"
+/home/minecraft/graalvm/graalvm-jdk-21.0.3+7.1/bin/java -jar ~/tools/mcaselector/mcaselector-2.4.2.jar --world ~/server/chromaticchaos2 "$@"
